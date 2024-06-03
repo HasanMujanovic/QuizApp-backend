2196fe3c4ebd4d332080c81c26277c30b81684a0
HasanMujanovic <respiratori1234@gmail.com>
2024-06-03 13:47:21 +0200

    first commit

---
+++ bitquiz/src/main/java/com/bitconex/bitquiz/BitquizApplication.java
@@ -0,0 +1,13 @@
+package com.bitconex.bitquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitquizApplication.class, args);
+	}
+
+}
---
+++ bitquiz/src/test/java/com/bitconex/bitquiz/BitquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitconex.bitquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
